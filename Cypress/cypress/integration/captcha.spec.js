8640e7971f9917492d19af11e18c5cfd3697fff8
ankit-pukale <ankitpukale@gmail.com>
2022-12-17 23:56:17 +0530

    captcha Testing using mysql

---
+++ Cypress/cypress/integration/captcha.spec.js
@@ -0,0 +1,23 @@
+///<reference types="cypress"/>
+describe('verify captcha using cypress',()=>{
+it('verify valid captcha',()=>{
+    let dbImageName=""
+    let dbImageSol=""
+    let UIImageName=""
+    let query="SELECT * FROM  captchadata WHERE imageName='$UIImageName';"
+    cy.visit('http://127.0.0.1:5555/index.html')
+    cy.get('#captchaImg').invoke('attr','imid').then((imName)=>{
+        UIImageName=imName
+        cy.log(UIImageName)
+    }).then(()=>{
+        cy.query(query.replace('$UIImageName',UIImageName)).then((dbres)=>{
+            dbImageSol=dbres[0].ImageSolution
+            cy.get('#capTextBox').type(dbImageSol,{force:true})
+            cy.screenshot()
+            cy.get('#btnSubmit').click()
+            cy.get('h1').should('have.text','Dashboard Page')
+        })
+    })
+    
+})
+})
